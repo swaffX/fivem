34884a5b74b4b2bfa8b39e250ac057bef6a4dfb2
swaffX <swaffedits.mp4@gmail.com>
2026-03-06 11:50:10 +0300

    gaegaeg

--- TXADMIN_BASLAT.sh
+++ TXADMIN_BASLAT.sh
@@ -7,8 +7,11 @@ echo "==================================="
 
 cd /root/fivem/alpine/opt/cfx-server
 
-# txAdmin modunda başlat
-./run.sh +set serverProfile nexora +set txAdminPort 40120 +set txAdminInterface 0.0.0.0
+# Cache klasörü oluştur
+[ -d cache ] || mkdir cache
+
+# txAdmin modunda başlat (direkt FXServer binary)
+./FXServer +set serverProfile nexora +set txAdminPort 40120 +set txAdminInterface 0.0.0.0
 
 # NOT: Bu komut çalıştığında terminal'de bir PIN kodu gösterilecek
 # Bu PIN'i tarayıcıda http://194.105.5.37:40120 adresinde kullanacaksınız
