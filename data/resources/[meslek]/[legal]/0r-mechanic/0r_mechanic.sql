959e0109f06f1a21600771c29aa1df265471ac76
swaffX <swaffedits.mp4@gmail.com>
2026-03-07 01:29:45 +0300

    aegaegaeg

---
+++ data/resources/[meslek]/[legal]/0r-mechanic/0r_mechanic.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS `0r_mechanics` (
+  `plate` text DEFAULT NULL,
+  `data` longtext DEFAULT '[]'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
--- data/resources/[meslek]/[legal]/kaves_mechanic/kaves_mechanic.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS `kaves_mechanics` (
-  `plate` text DEFAULT NULL,
-  `data` longtext DEFAULT '[]'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
